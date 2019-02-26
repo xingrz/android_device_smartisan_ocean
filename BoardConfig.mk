24c764b46e7340c867c7ea6295343b428c8648ea
XiNGRZ <chenxingyu92@gmail.com>
2019-02-26 16:17:03 +0800

    ocean: twrp: Append build date after version str

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,7 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEVICE_VERSION=$(shell date '+%Y%m%d.%H')
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_TWRPAPP := true
 TW_EXTRA_LANGUAGES := true
